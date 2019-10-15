3cd0f60d4ff16b7303a248139a012ddcb04b0ee1
Akula.Ramesh <rameshcdlv@gmail.com>
2019-10-15 15:15:37 +0530

    Updates

---
+++ precache-manifest.ba48dce87940011473cc8b510944b7e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9da73ab7814926ef8f690f08477bc15",
+    "url": "/react/-app/index.html"
+  },
+  {
+    "revision": "772e289f32d5ff11c382",
+    "url": "/react/-app/static/css/main.2be5bba8.chunk.css"
+  },
+  {
+    "revision": "bc236a2baae3cdaf5046",
+    "url": "/react/-app/static/js/2.32ccc0bf.chunk.js"
+  },
+  {
+    "revision": "772e289f32d5ff11c382",
+    "url": "/react/-app/static/js/main.c1a339ea.chunk.js"
+  },
+  {
+    "revision": "e1a877f6a2138c474181",
+    "url": "/react/-app/static/js/runtime-main.3543fb5a.js"
+  }
+]);
